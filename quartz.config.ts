faada9a31b6774e64ddb36b91504356d01e61023
Victor Liu <victorliu9001@gmail.com>
2024-05-18 01:39:17 -0400

    Quartz sync: May 18, 2024, 1:39 AM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "🪴 Victor Liu",
     enableSPA: true,
     enablePopovers: true,
     analytics: {
